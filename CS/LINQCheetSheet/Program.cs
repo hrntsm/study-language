9900bbefa7494d6aae05d63ced68d960a1a6526b
hrntsm <contact@hrntsm.com>
2021-03-09 11:48:53 +0900

    Add LINQCheetSheet

---
+++ CS/LINQCheetSheet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQCheetSheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foundation.Start();
+        }
+
+        public static void Write<T>(string tag, IEnumerable<T> e)
+        {
+            Console.WriteLine(tag.PadRight(10) + String.Join(",", e));
+        }
+
+        public static void Write<T>(string tag, T e)
+        {
+            Console.WriteLine(tag.PadRight(10) + e);
+        }
+    }
+
+    class Foundation
+    {
+        /// <summary>
+        /// 基本的なメソッド
+        /// </summary>
+        public static void Start()
+        {
+            var enumerable = Enumerable.Range(0, 10).ToArray();
+
+            // Where
+            var evens = enumerable.Where(i => i%2 == 1);
+            Program.Write("Where", evens);
+        }
+    }
+}
---
+++ CS/LINQCheetSheet/README.md
@@ -0,0 +1,5 @@
+# LINQ Cheet Sheet
+
+以下の記事を使って LINQ の勉強
+
+[LINQチートシート的なもの](https://qiita.com/Marimoiro/items/0e119b47d65bf138789a)
