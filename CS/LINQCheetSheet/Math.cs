e6388053f82ed5a44c4c2c4f853760c884442243
hrntsm <contact@hrntsm.com>
2021-03-13 14:54:51 +0900

    Add Math

---
+++ CS/LINQCheetSheet/Math.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace LINQCheetSheet
+{
+    class Math
+    {
+        public static void Start()
+        {
+            var enumerable = Enumerable.Range(0, 10).ToArray();
+
+            //Max
+            int max = enumerable.Max();
+            Program.Write("Max", max);
+
+            //Max after func
+            int max2 = enumerable.Max(i => i * i);
+            Program.Write("Max(f)", max2);
+
+            //Min
+            int min = enumerable.Min();
+            Program.Write("Min", min);
+
+            //Min after func
+            int min2 = enumerable.Min(i => i + 5);
+            Program.Write("Min(f)", min2);
+        }
+    }
+}
