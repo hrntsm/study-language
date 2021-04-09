a9461900fb0fcbcc31af072c6a36f1bca3de7251
hrntsm <contact@hrntsm.com>
2021-04-09 20:42:48 +0900

    Add material color

--- JS/Tutorial-threejs/IntroductionChapter/material_color/index.js
+++ JS/Tutorial-threejs/IntroductionChapter/material_color/index.js
@@ -13,4 +13,27 @@ function init() {
 
     const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
     camera.position.set(0, 0, +1000);
+
+    const geometry = new THREE.SphereGeometry(300, 30, 30);
+    const material = new THREE.MeshStandardMaterial({ color: 0xFF0000 });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const directioalLight = new THREE.DirectionalLight(0xFFFFFF);
+    directioalLight.position.set(1, 1, 1);
+    scene.add(directioalLight);
+
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    ambientLight.position.set(1, -1, 1);
+    scene.add(ambientLight);
+
+    tick();
+
+    function tick() {
+        mesh.rotation.y += 0.01;
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(tick);
+    }
 }
