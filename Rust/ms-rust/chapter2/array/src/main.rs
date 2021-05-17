a0d9b689b6b296467b1955c4d5ee1563f5eda5c9
hrntsm <contact@hrntsm.com>
2021-05-17 20:14:22 +0900

    Add array

---
+++ Rust/ms-rust/chapter2/array/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    println!("First weekday is {}", weekdays[0]);
+
+    let byte_buffer = [0_u8; 512];
+    println!("Buffer element 100 is {}", byte_buffer[99]);
+}
