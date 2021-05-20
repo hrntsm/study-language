9154aa35600e9920c05212286946d6f9b92933e8
hrntsm <contact@hrntsm.com>
2021-05-20 20:38:20 +0900

    Add result

---
+++ Rust/ms-rust/chapter3/result/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct DivisionByZeroError;
+
+fn safe_division(dividend: f64, divisor: f64) -> Result<f64, DivisionByZeroError> {
+    if divisor == 0.0 {
+        Err(DivisionByZeroError)
+    } else {
+        Ok(dividend / divisor)
+    }
+}
+
+fn main() {
+    println!("{:?}", safe_division(9.0, 3.0));
+    println!("{:?}", safe_division(4.0, 0.0));
+    println!("{:?}", safe_division(0.0, 3.0));
+}
