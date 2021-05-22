82be874f1a2f7b756d1b333ddfbbdb8200e0c8dd
hrntsm <contact@hrntsm.com>
2021-05-22 20:48:10 +0900

    Add exercise lifetime

---
+++ Rust/ms-rust/chapter4/exercise-lifetimes/src/main.rs
@@ -0,0 +1,21 @@
+fn copy_and_return<'a>(vector: &'a mut Vec<String>, value: &'a str) -> &'a String {
+    vector.push(String::from(value));
+    vector.get(vector.len() - 1).unwrap()
+}
+
+fn main() {
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, &name1));
+    assert_eq!("Chris", copy_and_return(&mut names, &name2));
+    assert_eq!("Anne", copy_and_return(&mut names, &name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
+}
