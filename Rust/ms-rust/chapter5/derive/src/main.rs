d523d3f76c7c55329fc8cbe8b81e1dab422f654a
hrntsm <contact@hrntsm.com>
2021-05-24 21:47:01 +0900

    Add derive

---
+++ Rust/ms-rust/chapter5/derive/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+use std::fmt;
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Point({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 4, y: -3 };
+
+    if p1 == p2 {
+        println!("equal!");
+    } else {
+        println!("not equal!");
+    }
+
+    println!("{}", p1);
+    println!("{:?}", p1);
+}
