363e1761cbf451a5c9f695802c9eb56122e0193f
hrntsm <contact@hrntsm.com>
2021-05-31 19:30:38 +0900

    Add test-xml-rs

---
+++ Rust/test-xml-rs/RAEDME.md
@@ -0,0 +1,5 @@
+# Test XML-rs
+
+crate のドキュメントを試す
+
+https://crates.io/crates/xml-rs
---
+++ Rust/test-xml-rs/src/main.rs
@@ -0,0 +1,45 @@
+extern crate xml;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn indent(size: usize) -> String {
+    const INDENT: &'static str = "    ";
+    (0..size)
+        .map(|_| INDENT)
+        .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
+}
+
+fn main() {
+    let file = File::open("file.stb").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => {
+                println!("{}+{}", indent(depth), name);
+                if attributes.len() > 0 {
+                    for attr in attributes {
+                        println!("{}  +attr: {}", indent(depth), attr.name);
+                    }
+                }
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{}-{}", indent(depth), name);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+            _ => {}
+        }
+    }
+}
