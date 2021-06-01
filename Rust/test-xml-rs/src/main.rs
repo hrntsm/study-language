f8124622bf4b0ac45529d2842e6cef766ccaeacd
hrntsm <contact@hrntsm.com>
2021-06-01 22:27:22 +0900

    Add output part

--- Rust/test-xml-rs/src/main.rs
+++ Rust/test-xml-rs/src/main.rs
@@ -1,9 +1,10 @@
 extern crate xml;
 
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{self, BufReader, Write};
 
 use xml::reader::{EventReader, XmlEvent};
+use xml::writer::{EmitterConfig, EventWriter, Result};
 
 fn indent(size: usize) -> String {
     const INDENT: &'static str = "    ";
@@ -12,8 +13,8 @@ fn indent(size: usize) -> String {
         .fold(String::with_capacity(size * INDENT.len()), |r, s| r + s)
 }
 
-fn main() {
-    let file = File::open("file.stb").unwrap();
+fn read_stb(path: &String) {
+    let file = File::open(path).unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
@@ -40,6 +41,48 @@ fn main() {
                 break;
             }
             _ => {}
+        }
+    }
+}
+
+// fn write_stb(path: &String) -> bool {}
+
+fn handle_event<W: Write>(w: &mut EventWriter<W>, line: String) -> Result<()> {
+    let line = line.trim();
+    let event: xml::writer::XmlEvent = if line.starts_with("+") && line.len() > 1 {
+        xml::writer::XmlEvent::start_element(&line[1..]).into()
+    } else if line.starts_with("-") {
+        xml::writer::XmlEvent::end_element().into()
+    } else {
+        xml::writer::XmlEvent::Characters(&line).into()
+    };
+    w.write(event)
+}
+
+fn main() {
+    let read_path = String::from("file_in.stb");
+    let write_path = String::from("file_out.stb");
+    read_stb(&read_path);
+    // write_stb(&write_path);
+
+    let mut file = File::create(write_path).unwrap();
+
+    let mut input = io::stdin();
+    let mut output = io::stdout();
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .create_writer(&mut file);
+    loop {
+        print!(">");
+        output.flush().unwrap();
+        let mut line = String::new();
+        match input.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => match handle_event(&mut writer, line) {
+                Ok(_) => {}
+                Err(e) => panic! {"Write error: {}", e},
+            },
+            Err(e) => panic!("Input error: {}", e),
         }
     }
 }
