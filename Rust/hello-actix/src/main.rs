3484f8400592f5b8470ca4f50fcfeb0d515e8313
hrntsm <contact@hrntsm.com>
2021-07-02 22:46:30 +0900

    Update

--- Rust/hello-actix/src/main.rs
+++ Rust/hello-actix/src/main.rs
@@ -1,12 +1,10 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError, http::header, post};
 use askama::Template;
-use thiserror::Error;
-
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
-
 use serde::Deserialize;
+use thiserror::Error;
 
 #[derive(Deserialize)]
 struct AddParams {
@@ -18,29 +16,6 @@ struct DeleteParams {
     id: u32,
 }
 
-#[post("/add")]
-async fn add_todo(
-    params: web::Form<AddParams>,
-    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
-) -> Result<HttpResponse, MyError> {
-    let conn = db.get()?;
-    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
-    Ok(HttpResponse::SeeOther()
-        .header(header::LOCATION, "/")
-        .finish())
-}
-
-#[post("/delete")]
-async fn delete_todo(
-    params: web::Form<DeleteParams>,
-    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
-) -> Result<HttpResponse, MyError> {
-    let conn = db.get()?;
-    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
-    Ok(HttpResponse::SeeOther()
-        .header(header::LOCATION, "/")
-        .finish())
-}
 struct TodoEntry {
     id: u32,
     text: String,
@@ -66,8 +41,34 @@ enum MyError {
    
 impl ResponseError for MyError {}
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[get("/")]
-async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+async fn index(
+    db: web::Data<Pool<SqliteConnectionManager>>
+) -> Result<HttpResponse, MyError> {
     let conn = db.get()?;
     let mut statement = conn.prepare("SELECT id, text FROM todo")?;
     let rows = statement.query_map(params![], |row| {
