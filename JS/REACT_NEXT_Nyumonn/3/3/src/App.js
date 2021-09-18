088effdfa63d90a4c575be8733a109c73107be1d
hrntsm <contact@hrntsm.com>
2021-09-18 17:35:21 +0900

    Update event bind

--- JS/REACT_NEXT_Nyumonn/3/3/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/3/src/App.js
@@ -5,15 +5,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      msg: "Hello", 
-      count: 0,
+      msg: "count start!",
+      counter: 0,
     };
-    let timer = setInterval(() => {
-      this.setState({
-        count: this.state.count + 1,
-        msg: "[ count: " + this.state.count + " ]"
-      })
-    }, 1000);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction() {
+    this.setState({
+      counter: this.state.counter + 1,
+      msg: '*** count: ' + this.state.counter + ' ***',
+    });
   }
 
   render() {
@@ -21,8 +23,12 @@ class App extends Component {
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <p className="subtitle">Count number</p>
-        <p className="alert alert-warning">{this.state.msg}</p>
-        <p className="alert alert-dark">{this.props.msg}</p>
+          <div className="alert alert-primary text-center">
+            <p className="h5 mb-4">{this.state.msg}</p>
+            <button className="btn btn-primary"
+              onClick={this.doAction}>
+              Click</button>
+          </div>
       </div>
     </div>
   }
