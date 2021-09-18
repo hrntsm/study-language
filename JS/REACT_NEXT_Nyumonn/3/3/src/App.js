0cc3e276ee9143a7a54b666c066a8df0a37e9d10
hrntsm <contact@hrntsm.com>
2021-09-18 18:40:42 +0900

    Update use map

--- JS/REACT_NEXT_Nyumonn/3/3/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/3/src/App.js
@@ -2,53 +2,72 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  data = [];
-
-  area = {
-    width: "500px",
-    height: "500px",
-    border: "1px solid blue",
-  }
+  data = [
+    "This is list sample.",
+    "これはリストのサンプルです",
+    "配列をリストに変換します",
+  ];
 
   constructor(props) {
     super(props);
     this.state = {
       list: this.data,
     };
-    this.doAction = this.doAction.bind(this);
-  }
-
-  doAction(e) {
-    let x = e.pageX;
-    let y = e.pageY;
-    this.data.push({ x: x, y: y });
-    this.setState({
-      list: this.data,
-    });
-  }
-
-  draw(d) {
-    let s = {
-      position: "absolute",
-      left: (d.x - 25) + "px",
-      top: (d.y - 25) + "px",
-      width: "50px",
-      height: "50px",
-      backgroundColor: "#66f3",
-    }
-    return <div style={s}></div>
   }
 
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle</p>
+        <p className="subtitle">Show List.</p>
         <div style={this.area} onClick={this.doAction}>
-          {this.data.map((value) => this.draw(value))}
+          <List title="サンプル リスト" data={this.data} />
         </div>
       </div>
     </div>
+  }
+}
+
+class List extends Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <div>
+        <p className="h5 text-center">{this.props.title}</p>
+        <ul className="list-group">
+          {data.map((item, key) =>
+            <li className="list-group" key={key}>
+              <Item number={key + 1} value={item} />
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+class Item extends Component {
+  itm = {
+    fontSize: "16pt",
+    color: "#00f",
+    textDecoration: "underline",
+    textDecorationColor: "#ddf",
+  };
+
+  num = {
+    fontWeight: "bold",
+    color: "red",
+  }
+
+  render() {
+    return (
+      <p style={this.itm}>
+        <span style={this.num}>
+          [{this.props.number}]&nbsp;
+        </span>
+        {this.props.value}
+      </p>
+    )
   }
 }
 
