6efa79dd33f8a579c0f3f320cf9648b7ffcbef87
hrntsm <contact@hrntsm.com>
2021-09-18 18:14:41 +0900

    Update use props and state

--- JS/REACT_NEXT_Nyumonn/3/3/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/3/src/App.js
@@ -2,43 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  data = [];
+
+  area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid blue",
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      msg: "count start!",
-      counter: 0,
-      flg: true,
+      list: this.data,
     };
     this.doAction = this.doAction.bind(this);
   }
 
-  doAction() {
+  doAction(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({ x: x, y: y });
     this.setState({
-      counter: this.state.counter + 1,
-      msg: '*** count: ' + this.state.counter + ' ***',
-      flg: !this.state.flg,
+      list: this.data,
     });
+  }
+
+  draw(d) {
+    let s = {
+      position: "absolute",
+      left: (d.x - 25) + "px",
+      top: (d.y - 25) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    }
+    return <div style={s}></div>
   }
 
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">Count number</p>
-        {this.state.flg ?
-          <div className="alert alert-warning text-left">
-            <p className="h5">count: {this.state.msg}</p>
-          </div>
-          :
-          <div className="alert alert-success text-left">
-            <p className="h5">{this.state.msg}です。</p>
-          </div>
-        }
-        <div className="text-center">
-          <button className="btn btn-primary"
-            onClick={this.doAction}>
-            Click</button>
-          </div>
+        <p className="subtitle">draw rectangle</p>
+        <div style={this.area} onClick={this.doAction}>
+          {this.data.map((value) => this.draw(value))}
+        </div>
       </div>
     </div>
   }
