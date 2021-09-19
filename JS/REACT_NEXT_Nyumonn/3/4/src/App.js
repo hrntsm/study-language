d8563030d5e90d569986ef2239bd0b2800741592
hrntsm <contact@hrntsm.com>
2021-09-19 21:53:11 +0900

    Update use context to change theme

--- JS/REACT_NEXT_Nyumonn/3/4/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/4/src/App.js
@@ -1,56 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let data = {
-  title: 'React-Context',
-  message: 'this is sample message.'
+let theme = {
+  light: {
+    styles: {
+      backgroundColor: "#f0f9ff",
+      color: "#00f",
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4"
+  },
+  dark: {
+    styles: {
+      backgroundColor: "#336",
+      color: "#eef",
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4"
+  },
 }
 
-const SampleContext = React.createContext(data);
+const ThemeContext = React.createContext(theme.dark);
 
 class App extends Component {
-  newdata = {
-    title: '新しいタイトルです',
-    message: '新しいメッセージです'
-  }
+  static contextType = ThemeContext;
 
   render() {
-    return <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+    return <div style={this.context.styles}>
+      <h1 className={this.context.head}>React</h1>
       <div className="container">
-        <Title />
-        <Message />
-        <hr />
-        <SampleContext.Provider value={this.newdata}>
-          <Title />
-          <Message />
-        </SampleContext.Provider>
-        <hr />
-        <Title />
-        <Message />
+        <Title value="Context page" />
+        <Message value="This is Context sample." />
+        <Message value="これはテーマのサンプルです。" />
+        <div ClassName={this.context.foot}></div>
       </div>
     </div>
   }
 }
 
 class Title extends Component {
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render() {
     return (
-      <div className="card p-2 my-3">
-        <h2>{this.context.title}</h2>
+      <div className={this.context.alert}>
+        <h2 style={this.context.styles}>{this.props.value}</h2>
       </div>
     )
   }
 }
 
 class Message extends Component {
-  static contextType = SampleContext;
+  static contextType = ThemeContext;
 
   render() {
-    return <div className="alert alert-primary">
-      <p>{this.context.message}</p>
+    return <div style={this.context.styles}>
+      <p className={this.context.text}>{this.props.value}</p>
     </div>
   }
 }
