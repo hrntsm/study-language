b4046448b6a5837db9889617d1fdfea33352d4b0
hrntsm <contact@hrntsm.com>
2021-09-19 21:31:00 +0900

    Update use context

--- JS/REACT_NEXT_Nyumonn/3/4/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/4/src/App.js
@@ -1,58 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let data = {
+  title: 'React-Context',
+  message: 'this is sample message.'
+}
+
+const SampleContext = React.createContext(data);
+
 class App extends Component {
-  input = '';
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'input form',
-      message: 'type your name.',
-      max: 10,
-    }
-    this.doCheck = this.doCheck.bind(this);
-  };
-
-  doCheck(e) {
-    alert(e.target.value + "は長すぎます。（最大" + this.state.max + "文字）");
-  }
-
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4>{this.state.title}</h4>
-        <Message maxlength={this.state.max} onCheck={this.doCheck} />
+        <Title />
+        <Message />
       </div>
     </div>
   }
 }
 
-
-class Message extends Component {
-  li = {
-    fontSize: "14px",
-    fontWeight: "bold",
-    color: "##090"
-  }
-
-  constructor(props) {
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e) {
-    if (e.target.value.length > this.props.maxlength) {
-      this.props.onCheck(e);
-      e.target.value = e.target.value.substr(0, this.props.maxlength);
-    }
-  }
+class Title extends Component {
+  static contextType = SampleContext;
 
   render() {
-    return <div className="form-group">
-      <label>input:</label>
-      <input type="text" className="form-control" onChange={this.doChange} />
+    return (
+      <div className="card p-2 my-3">
+        <h2>{this.context.title}</h2>
+      </div>
+    )
+  }
+}
+
+class Message extends Component {
+  static contextType = SampleContext;
+
+  render() {
+    return <div className="alert alert-primary">
+      <p>{this.context.message}</p>
     </div>
   }
 }
