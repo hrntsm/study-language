44c5b50e46ed8299449f3f55acb3e8c22dfed970
hrntsm <contact@hrntsm.com>
2021-09-19 20:45:54 +0900

    Update use input form

--- JS/REACT_NEXT_Nyumonn/3/4/src/App.js
+++ JS/REACT_NEXT_Nyumonn/3/4/src/App.js
@@ -4,43 +4,46 @@ import './App.css';
 class App extends Component {
   input = '';
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'input form',
+      message: 'type your name.'
+    }
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+  };
+
+  doChange(e) {
+    this.input = e.target.value;
+  }
+
+  doSubmit(e) {
+    this.setState({
+      title: 'send from',
+      message: 'Hello, ' + this.input + '!!',
+    })
+    e.preventDefault();
+  }
+
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <Message title="Children!">
-          これはコンポーネント内のコンテンツです。
-          句点でテキストを分割し、リストにして表示しています。
-          改行は必要ありません。
-        </Message>
+        <h4>{this.state.title}</h4>
+        <p className="card h5 p-3">{this.state.message}</p>
+        <div className="alert alert-primary mt-3">
+          <form onSubmit={this.doSubmit}>
+            <div className="form-group">
+              <label>Message:</label>
+              <input type="text" className="form-control"
+                onChange={this.doChange}
+                required pattern="[A-Za-z _,.]+" />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Click" />
+          </form>
+        </div>
       </div>
-    </div>
-  }
-}
-
-class Message extends Component {
-  li = {
-    fontSize: '14pt',
-    fontWeight: 'bold',
-    color: '#090',
-  }
-
-  render() {
-    let contents = this.props.children;
-    let arr = contents.split('。');
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].trim() !== '') {
-        arr2.push(arr[i]);
-      }
-    }
-    let list = arr2.map((value, key) => (
-      <li className="list-group-item" style={this.li} key={key}>{key + 1}. {value} .</li>)
-    )
-
-    return <div>
-      <h2>{this.props.title}</h2>
-      <ol className="list-group">{list}</ol>
     </div>
   }
 }
