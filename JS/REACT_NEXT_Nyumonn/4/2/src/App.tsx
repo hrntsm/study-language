2a124ae9067765e9d001438381e83bb01182668b
hrntsm <contact@hrntsm.com>
2021-09-20 16:20:39 +0900

    Update hook use props interaction

--- JS/REACT_NEXT_Nyumonn/4/2/src/App.tsx
+++ JS/REACT_NEXT_Nyumonn/4/2/src/App.tsx
@@ -2,35 +2,52 @@ import React, { useState } from 'react';
 import './App.css';
 
 function AlertMessage(props: any) {
+  const data = ["Hello!", "Welcome...", "Good-bye?"]
+
+  const actionAlert = () => {
+    const re = data[Math.floor(Math.random() * data.length)]
+    props.setAlert("message: " + re + ".")
+  }
+
   return <div className="alert alert-primary h5 text-primary">
-    {props.message}
+    <h5>{props.alert}</h5>
+    <button onClick={actionAlert} className="btn btn-primary">
+      Click me!
+    </button>
   </div>
 }
 
 function CardMessage(props: any) {
+  const [count, setCount] = useState(0)
+
+  const actionCard = () => {
+    setCount(count + 1)
+    props.setCard("card counter: " + count + " count.")
+  }
+
   return <div className="card p-3 h5 border-primary text-center">
-    {props.message}
+    <h5>{props.card}</h5>
+    <button onClick={actionCard} className="btn btn-secondary">
+      Click me!
+    </button>
   </div>
 }
 
 function App() {
-  const [msg, setMsg] = useState('This is sample message!');
-  const doAction = () => {
-    let res = window.prompt('type your name:')
-    setMsg("Hello, " + res + "!!")
-  }
-  
+  const [alert, setAlert] = useState("This is alert message!")
+  const [card, setCard] = useState("This is card message!")
+
   return (
     <div>
       <h1 className="bg-primary text-white display-4 text-right">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage message={msg}/>
-        <CardMessage message={msg}/>
-        <div className="text-center">
-          <button onClick={doAction} className="btn btn-primary">
-            Click me!!
-          </button>
+        <AlertMessage alert={alert} setAlert={setAlert} />
+        <CardMessage card={card} setCard={setCard} />
+        <hr />
+        <div className="text-right">
+          <p>{alert}</p>
+          <p>{card}</p>
         </div>
       </div>
     </div>
