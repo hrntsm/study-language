5fed46ca7cd817ad6243866606050a1d49df6c0b
hrntsm <contact@hrntsm.com>
2021-09-20 12:35:09 +0900

    Update use useState

--- JS/REACT_NEXT_Nyumonn/4/1/src/App.tsx
+++ JS/REACT_NEXT_Nyumonn/4/1/src/App.tsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message] = useState("Welcome to Hook!")
+  const [count, setCount] = useState(0);
+  const clickFunc = () => {
+    setCount(count + 1);
+  }
 
   return (
     <div>
@@ -10,7 +13,10 @@ function App() {
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
         <div className="alert alert-primary text-center">
-          <p className="h5">{message}</p>
+          <p className="h5 mb-3">click: {count} times!</p>
+          <div>
+            <button className="btn btn-primary" onClick={clickFunc}>Click me!</button>
+          </div>
         </div>
       </div>
     </div>
