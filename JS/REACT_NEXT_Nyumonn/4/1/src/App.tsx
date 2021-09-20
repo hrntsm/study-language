150e4de3a3befeca704ee3201203dd9f39e9a295
hrntsm <contact@hrntsm.com>
2021-09-20 13:01:09 +0900

    Update use hook changeflag

--- JS/REACT_NEXT_Nyumonn/4/1/src/App.tsx
+++ JS/REACT_NEXT_Nyumonn/4/1/src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
   const clickFunc = () => {
     setCount(count + 1);
+  }
+  const changeFlag = (e: any) => {
+    setFlag(e.target.checked);
   }
 
   return (
@@ -12,11 +16,28 @@ function App() {
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-3">click: {count} times!</p>
-          <div>
-            <button className="btn btn-primary" onClick={clickFunc}>Click me!</button>
+        {flag ?
+          <div className="alert alert-primary text-center">
+            <p className="h5 mb-3">click: {count} times!</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>
+                Click me!
+              </button>
+            </div>
           </div>
+          :
+          <div className="card p-3 border-primary text-center">
+            <p className="h5 mb-3 text-left text-primary">click: {count} times!</p>
+            <div>
+              <button className="btn btn-primary" onClick={clickFunc}>
+                Click me!
+              </button>
+            </div>
+          </div>
+        }
+        <div className="form-group h6 text-center pt-3">
+          <input type="checkbox" className="form-check-input" id="check1" onChange={changeFlag} />
+          <label className="form-check-label" htmlFor="check1">Change from style.</label>
         </div>
       </div>
     </div>
