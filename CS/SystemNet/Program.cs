e08a86ddb3a45c1f98a1f23049f2208991e373c9
hrntsm <contact@hrntsm.com>
2021-09-22 23:15:01 +0900

    Add SystemNet

---
+++ CS/SystemNet/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace TestSystemNet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private string Http(string method, string url, string dataString, Dictionary<string, string> headers, int retry = 3)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.UseDefaultCredentials = true;
+            request.Method = method;
+            request.ContentLength = 0;
+
+            foreach (KeyValuePair<string, string> header in headers)
+            {
+                request.Headers.Add(header.Key, header.Value);
+            }
+
+            if (dataString != null)
+            {
+                request.ContentType = "application/json";
+                var encording = Encoding.UTF8;
+                byte[] data = encording.GetBytes(dataString);
+                request.ContentLength = data.Length;
+
+                using var stream = request.GetRequestStream();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+            }
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                var reader = new StreamReader(dataStream);
+
+                string responseFromServer = reader.ReadToEnd();
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                return responseFromServer;
+            }
+            catch (WebException ex)
+            {
+                // exception のとき何が返ってきているか確認する。
+                // 参考では以下になっている
+                // e.Response.StatusDescription == "Not Found"
+                if (ex.Response == null || retry > 0)
+                {
+                    Thread.Sleep(1000);
+                    return Http(method, url, dataString, headers, retry - 1);
+                }
+
+                Stream dataStream = ex.Response.GetResponseStream();
+                var reader = new StreamReader(dataStream);
+
+                string responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+
+                return responseFromServer;
+            }
+        }
+    }
+}
---
+++ CS/SystemNet/README.md
@@ -0,0 +1,7 @@
+# Test System.Net
+
+HTTP のリクエスト方法を確認するため、 Notion API との連携を参考に作成
+
+[![](https://img.youtube.com/vi/NJnM935Jfnk/0.jpg)](https://www.youtube.com/watch?v=NJnM935Jfnk)
+
+https://www.notion.so/Getting-Started-with-NotionAPI-8dbc03801bf54d23b4ffded7d7981a73#8c7244b976b84eafad7c0cecf2feb365
